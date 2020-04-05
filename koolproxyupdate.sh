2eaa69a92b4960cd0cfbdada68198c0669ba68de
ycg31 <ycg31@qq.com>
2020-04-05 17:23:46 +0800

    添加规则更新代码

--- koolproxyupdate.sh
+++ koolproxyupdate.sh
@@ -1,7 +1,9 @@
-#wget 'https://kprule.com/kp.dat' -O files/usr/share/koolproxy/data/rules/kp.dat
-#wget 'https://kprule.com/koolproxy.txt' -O files/usr/share/koolproxy/data/rules/koolproxy.txt
-#wget 'https://kprule.com/daily.txt' -O files/usr/share/koolproxy/data/rules/daily.txt
-#wget 'https://kprule.com/user.txt' -O files/usr/share/koolproxy/data/user.txt
+wget 'https://gitee.com/ycg31/KoolProxy/raw/master/rules/kp.dat' -O files/usr/share/koolproxy/data/rules/kp.dat
+wget 'https://gitee.com/ycg31/KoolProxy/raw/master/rules/koolproxy.txt' -O files/usr/share/koolproxy/data/rules/koolproxy.txt
+wget 'https://gitee.com/ycg31/KoolProxy/raw/master/rules/daily.txt' -O files/usr/share/koolproxy/data/rules/daily.txt
+wget 'https://gitee.com/ycg31/KoolProxy/raw/master/rules/user.txt' -O files/usr/share/koolproxy/data/user.txt
+cp files/usr/share/koolproxy/data/user.txt files/usr/share/koolproxy/data/rules/user.txt
+wget 'https://gitee.com/ycg31/KoolProxy/raw/master/rules/koolproxy_ipset.conf' -O files/usr/share/koolproxy/koolproxy_ipset.conf
 #wget 'https://koolproxy.com/downloads/arm' -O files/bin/arm
 #wget 'https://koolproxy.com/downloads/i386' -O files/bin/i386
 #wget 'https://koolproxy.com/downloads/mips' -O files/bin/mips
